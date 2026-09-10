0ccaaad43246d7641cbb698a51f34fe8a022be8c
Antonio Rabago <anrabago@gmail.com>
2026-09-10 14:31:33 +0200

    corregir kaboom_1.py para que se muestren los print iniciales

--- module06/ft_kaboom_1.py
+++ module06/ft_kaboom_1.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-from alchemy.grimoire.dark_spellbook import dark_spell_record
-
 
 def main() -> None:
     print("=== Kaboom 1 ===")
     print("Access to alchemy/grimoire/dark_spellbook.py directly")
     print("Test import now- THIS WILL RAISE AN UNCAUGHT EXCEPTION")
+    from alchemy.grimoire.dark_spellbook import dark_spell_record
     record = dark_spell_record(
             'Fantasy',
             'Earth, wind and fire'
